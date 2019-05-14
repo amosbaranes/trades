60cedf9cba4026ef9e35d98184a4fad6ac7b0e33
amos baranes <amos@drbaranes.com>
2019-05-14 21:24:01 +0300

    a

--- web/trades/settings/base.py
+++ web/trades/settings/base.py
@@ -29,7 +29,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'trades.middleware.LoginRequiredMiddleware',
+    'trades.middleware.LoginRequiredMiddleware',
 )
 
 ROOT_URLCONF = 'trades.urls'
